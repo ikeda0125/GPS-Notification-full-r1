88ea90dc181290666cf35f75ddf28a3ce5a961ff
ikeda0125 <32475655+ikeda0125@users.noreply.github.com>
2021-08-16 20:55:06 +0900

    commit

--- sampleNotification/AppDelegate.swift
+++ sampleNotification/AppDelegate.swift
@@ -3,17 +3,33 @@
 //  sampleNotification
 //
 //  Created by 池田佳弘 on 2021/08/14.
-//
+//  参考：https://i-app-tec.com/ios/time-interval.html
 
 import UIKit
+import UserNotifications
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // notification center (singleton)
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+
+        // ------------------------------------
+        // 前準備: ユーザに通知の許可を求める
+        // ------------------------------------
+        // request to notify for user
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Allowed")
+            } else {
+                print("Didn't allowed")
+            }
+        }
+        
         return true
     }
 
@@ -30,7 +46,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 通知が到着した時の挙動
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // 音で通知が到着したことを知らせる
+        completionHandler([[.sound]])
+    }
+}
--- sampleNotification/ViewController.swift
+++ sampleNotification/ViewController.swift
@@ -1,19 +1,72 @@
-//
-//  ViewController.swift
-//  sampleNotification
-//
-//  Created by 池田佳弘 on 2021/08/14.
-//
-
 import UIKit
+import CoreLocation
+import UserNotifications
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager: CLLocationManager = CLLocationManager()
+                    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        locationManager.distanceFilter = 100
+        
+        let geoFenceRegion:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(35.7020691, 139.7753269),
+                                                               radius: 10,
+                                                               identifier: "Base")
+        
+        locationManager.startMonitoring(for: geoFenceRegion)
     }
+    
+    // 現在位置を表示
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("緯度: ", location.coordinate.latitude, "経度: ", location.coordinate.longitude)
+        }
+    }
+    
+    // 指定エリアに入った時に発火する。
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("Entered: /(region.identifier)")
+        setNotification()
+    }
+    // debug用。指定エリアから出た時に発火する。
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("Exited: /(region.identifier)")
+        //setNotification()
+    }
+    
+    func setNotification() {
+        let seconds = 5
 
+        // ------------------------------------
+        // 通知の発行: タイマーを指定して発行
+        // ------------------------------------
+        // content
+        let content = UNMutableNotificationContent()
+        content.title = "お相手が近くにいます"
+        //content.subtitle = "通知からアプリを開いて、お相手を探しましょう！！"
+        content.body = "通知からアプリを開いて、お相手を探しましょう！！"
+        content.sound = UNNotificationSound.default
 
+        // trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: Double(seconds),
+                                                        repeats: false)
+
+        // request includes content & trigger
+        let request = UNNotificationRequest(identifier: "TIMER\(seconds)",
+                                            content: content,
+                                            trigger: trigger)
+
+        // schedule notification by adding request to notification center
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
-
